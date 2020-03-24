b62949c0bd807624ffdbc7907d75d6b9c53d1054
Matteo Formentin <matteo.formentin@icloud.com>
2020-03-24 16:28:28 +0100

    Fix.

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ READ ENV FROM FILE
 Local development only: on container ENV are directly provided on start by docker.
 */
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 uuidv4 = require("uuid/v4");
 crypto = require("crypto");
